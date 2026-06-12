a5965b6725de19244fa1dc53d36dc2fea066b1f2
Tvoy Nickname <tvoya.pochta@example.com>
2026-06-12 17:22:34 +0300

    Expose native target through core dependency

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .library(
             name: "DroidShimCore",
             type: .dynamic,
-            targets: ["DroidShimCore", "DroidShimNative"]
+            targets: ["DroidShimCore"]
         ),
         .executable(
             name: "DroidShimApp",
